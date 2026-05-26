d09f0cfb410385c1e90896a39fe44c7501e76920
null <null>
2026-05-26 18:12:39 +0900

    Styled JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
